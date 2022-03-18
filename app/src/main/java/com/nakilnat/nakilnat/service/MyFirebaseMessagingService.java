16952ef96f389b245cc9aef809839e752fc33851
firatyildizhan <hanyildizfirat@gmail.com>
2022-03-18 12:37:10 +0300

    add push notification

---
+++ app/src/main/java/com/nakilnat/nakilnat/service/MyFirebaseMessagingService.java
@@ -0,0 +1,6 @@
+package com.nakilnat.nakilnat.service;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+}
